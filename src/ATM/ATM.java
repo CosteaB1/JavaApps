be328c598a1581bf303c0d10abaf691e7d432651
CosteaB1 <50628796+CosteaB1@users.noreply.github.com>
2020-08-18 18:07:37 +0300

    Login Successfully/ need to check on Wrong pass/username

--- src/ATM/ATM.java
+++ src/ATM/ATM.java
@@ -9,20 +9,7 @@ public class ATM {
     public static void main(String[] args) {
 
         DataBase test1 = new DataBase();
-
         test1.connectionDB();
-
-//        Scanner keyboard = new Scanner(System.in);
-//        System.out.println("Enter Username: ");
-//        String username = keyboard.nextLine();
-//        System.out.println("Enter Pass: ");
-//        String pass = keyboard.nextLine();
-//
-//        if (username.equals("Costea") && (pass.equals("1234"))) {
-//            System.out.println("Login Successfully");
-//        } else {
-//            System.out.println("Login or password is incorrect");
-//        }
 
 
     }
--- src/ATM/DataBase.java
+++ src/ATM/DataBase.java
@@ -1,6 +1,7 @@
 package ATM;
 
 import java.sql.*;
+import java.util.Scanner;
 
 
 public class DataBase {
@@ -15,10 +16,12 @@ public class DataBase {
 
             conn1 = DriverManager.getConnection(url1, user, password);
             Statement stmt = conn1.createStatement();
-            checkLogin(stmt);
             if (conn1 != null) {
                 System.out.println("Connected to the database ebookshop: " + url1 + " username:" + user);
             }
+
+            checkLogin(stmt);
+
         } catch (SQLException e) {
             System.out.println("An error occurred. Maybe user/password is invalid");
             e.printStackTrace();
@@ -27,19 +30,55 @@ public class DataBase {
 
 
     public void checkLogin(Statement stmt) throws SQLException {
+        String[] username_1 = new String[10];
+        String[] pass_1 = new String[10];
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter Username: ");
+        String username = keyboard.nextLine();
+        System.out.println("Enter Pass: ");
+        String pass = keyboard.nextLine();
 
-        String strSelect = "SELECT userName, pass FROM users";
-        System.out.println("The SQL statement is: " + strSelect + "\n");
+        String strSelect = "SELECT * FROM users";
+//        System.out.println("The SQL statement is: " + strSelect + "\n");
         ResultSet rset = stmt.executeQuery(strSelect);
-
-        while(rset.next()) {
-            String username_1 = rset.getString("userName");
-            String pass_1 = rset.getString("pass");
-            System.out.println(username_1 + ", " + pass_1);
-
+        for (int i = 0; i < 3; i++) {
+            if (rset.next()) {
+                username_1[i] = rset.getString("userName");
+                pass_1[i] = rset.getString("pass");
+                System.out.println(username_1[i] + " " + pass_1[i]);
+            }
         }
+        for (int i = 0; i < 3; i++) {
+            if (username.equals(username_1[i]) && pass.equals(pass_1[i])) {
+                System.out.println("Login Successfully");
+                break;
+            }
+            // Need to solve on Error
+        }
+
 
     }
 
 
+//        for (int i = 0; i < 3; i++) {
+//        while (rset.getString(int j)) {
+//                username_1[i] = rset.getString("userName");
+//                pass_1[i] = rset.getString("pass");
+//                System.out.println(username_1[i] + " " + pass_1[i]);
+//            }
+//
+//        }
+
+//        for (int i = 0; i < 1; i++) {
+//            if (username.equals(username_1[i]) && (pass.equals(pass_1[i]))) {
+//                System.out.println("Login Successfully");
+//            } else {
+//                System.out.println("Login or password is incorrect");
+//            }
+//        }
+
+
 }
+
+
+
