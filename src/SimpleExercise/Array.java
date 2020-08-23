12619b8befaa144a54a761f08054b2cb5ebe95e1
CosteaB1 <50628796+CosteaB1@users.noreply.github.com>
2020-08-23 16:35:47 +0300

    some Array execises

--- src/SimpleExercise/Array.java
+++ src/SimpleExercise/Array.java
@@ -1,146 +1,29 @@
 package SimpleExercise;
-
-import java.util.Arrays;
-import java.util.Random;
-
-public class Array {
+public class Array extends Exercises {
 
 
     public static void main(String[] args) {
-        int[] intArr = {5, 3, 2, 5, 7, 4, 3, 2};
-        int[] newArr = new int[intArr.length];
-//        System.out.println("Index position of 3 is: " + findIndex(intArr, 3));
-//        removeElement(intArr, 3);
-        copyArray(intArr, newArr);
+
+//        int[] newArr = new int[intArr.length];
+////        System.out.println("Index position of 3 is: " + findIndex(intArr, 3));
+////        removeElement(intArr, 3);
+////        copyArray(intArr, newArr);
+//        insertSpecificPosition(intArr,1,100 );
+//        int arr[] = {1,2,2,3,4,4,4,5,2,2,3,4,1};
+//        int n = arr.length;
+//        n = removeDuplicates(arr,n);
+//        for (int i=0; i<n; i++)
+//            System.out.print(arr[i]+" ");
+//        String s = "aaaaabbbbbccccdddd";
+//        prinRLE(s);
+//        int number = 5, result;
+//        result = factorial(number);
+//        System.out.println("factorial of: " + number + " is: " + result);
+
+        int[] myArr = {1,2,3,4,5,6,7,8,9,10};
+//        maxMinArray(myArr);
+        reverse(myArr);
     }
 
-    //9. Write a Java program to insert an element (specific position) into an array. Go to the editor
-
-    public static void insertSpecificPosition(int[] intArr, int position) {
-        
-    }
-
-    //8. Write a Java program to copy an array by iterating the array. Go to the editor
-
-    public static void copyArray(int[] intArr, int[] newArr) {
-        System.out.println("My array: " + Arrays.toString(intArr));
-        for (int i = 0; i < intArr.length; i++) {
-            newArr[i] = intArr[i];
-        }
-        System.out.println("New Array (copy) : " + Arrays.toString(newArr));
-    }
-
-    //7. Write a Java program to remove a specific element from an array. Go to the editor
-
-    public static void removeElement(int[] intArr, int removeIndex) {
-        System.out.println("Original Array of int: ");
-        for (int i = 0; i < intArr.length; i++) {
-            System.out.print(intArr[i] + " ");
-        }
-
-        System.out.println("\nIndex of element to be removed: " + removeIndex);
-
-        if (intArr == null) {
-            System.out.println("The list is empty!!");
-        }
-
-        int[] newArray = new int[intArr.length - 1];
-        for (int i = 0, k = 0; i < intArr.length; i++) {
-            if (i == removeIndex) {
-                continue;
-            } else {
-                newArray[k++] = intArr[i];
-            }
-        }
-        System.out.println("Array after removing index of element: " + Arrays.toString(newArray));
-
-
-    }
-
-    //    6. Write a Java program to find the index of an array element
-    public static int findIndex(int[] intArr, int t) {
-        if (intArr == null) {
-            return -1;
-        }
-        int len = intArr.length;
-        int i = 0;
-        while (i < len) {
-            if (intArr[i] == t) {
-                return i;
-            } else i++;
-        }
-        return -1;
-    }
-
-
-    //  5. Write a Java program to test if an array contains a specific value.
-    public static boolean contains(int[] intArr, int item) {
-        for (int n : intArr) {
-            if (item == n) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-//        //4. Write a Java program to calculate the average value of array elements.
-//        int[] intArr = {5,4,3,5};
-//        int sum = 0;
-//        int elements = 0;
-//        double average;
-//
-//        for (int i =0 ; i < intArr.length ; i++) {
-//            int temp = intArr[i];
-//            sum+=temp;
-//            elements++;
-//        }
-//        average = sum / elements;
-//        System.out.println(average);
-
-
-////Write a Java program to sum values of an array
-//        int[] intArr = {5,2,3,4,6};
-//        int sum =0 ;
-//
-//        for (int i =0 ; i < intArr.length; i++) {
-//            int temp = intArr[i];
-//            sum +=temp;
-//        }
-//        System.out.println(sum);
-
-
-//        //Sort String Array
-//        String[] myArray = {"JavaFX", "HBase", "OpenCV", "Java", "Hadoop", "Neo4j"};
-//        int size = myArray.length;
-//
-//        for(int i = 0; i<size-1; i++) {
-//            for (int j = i+1; j<myArray.length; j++) {
-//                if(myArray[i].compareTo(myArray[j])>0) {
-//                    String temp = myArray[i];
-//                    myArray[i] = myArray[j];
-//                    myArray[j] = temp;
-//                }
-//            }
-//        }
-//        System.out.println(Arrays.toString(myArray));
-//        //using Sort method of the Arrays class
-//        Arrays.sort(myArray);
-//        System.out.println(Arrays.toString(myArray));
-//
-//        //Sort Numberic Array (Bubble sort)
-//        int[] myInt = {6,5,3,2,5,7,1,2,4,53,2};
-//
-//        for (int i =0 ; i < myInt.length-1 ; i++) {
-//            for (int j=0; j< myInt.length-i-1; j++) {
-//                if(myInt[j] > myInt[j+1]) {
-//                    int temp = myInt[j];
-//                    myInt[j] = myInt[j+1];
-//                    myInt[j+1] = temp;
-//                }
-//            }
-//        }
-//        for(int i =0 ; i < myInt.length ; i++) {
-//            System.out.print(" " + myInt[i]);
-//        }
 }
 
---
+++ src/SimpleExercise/Exercises.java
@@ -0,0 +1,218 @@
+package SimpleExercise;
+
+import java.util.Arrays;
+
+public class Exercises {
+    //11. Write a Java program to reverse an array of integer values. Go to the editor
+
+    public static void reverse(int[] myArr) {
+        System.out.println("Original String : " + Arrays.toString(myArr));
+        int n = myArr.length;
+        for (int i = 0; i < n / 2; i++) {
+            int temp = myArr[i];
+            myArr[i] = myArr[n - i - 1];
+            myArr[n - i - 1] = temp;
+        }
+        System.out.println("Reversed String : " + Arrays.toString(myArr));
+    }
+
+    //10. Write a Java program to find the maximum and minimum value of an array. Go to the editor
+    public static void maxMinArray(int[] myArr) {
+        int n = myArr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (myArr[j] > myArr[j + 1]) {
+                    int temp = myArr[j];
+                    myArr[j] = myArr[j + 1];
+                    myArr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Min element is: " + myArr[0] + "\nMax element is: " + myArr[n - 1]);
+    }
+
+    //11.run length encoding
+
+    public static void prinRLE(String str) {
+        int n = str.length();
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            while (i < n - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            System.out.print(str.charAt(i));
+            System.out.print(count);
+        }
+    }
+
+
+    //10. Take the dublicates from an array:
+    static int removeDuplicates(int arr[], int n) {
+        // Return, if array is empty
+        // or contains a single element
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int[] temp = new int[n];
+        // Start traversing elements
+        int j = 0;
+        for (int i = 0; i < n - 1; i++)
+            // If current element is not equal
+            // to next element then store that
+            // current element
+
+            if (arr[i] != arr[i + 1])
+                temp[j++] = arr[i];
+
+        // Store the last element as whether
+        // it is unique or repeated, it hasn't
+        // stored previously
+        temp[j++] = arr[n - 1];
+
+        // Modify original array
+        for (int i = 0; i < j; i++)
+            arr[i] = temp[i];
+
+        return j;
+
+
+    }
+
+    //9. Find Factorial of a number with recursion
+    static int factorial(int n) {
+        if (n != 0) {
+            return n * factorial(n - 1);
+        } else return 1;
+    }
+
+    public static void insertSpecificPosition(int[] intArr, int index, int value) {
+        System.out.println("My array: " + Arrays.toString(intArr));
+        int[] newArray = new int[intArr.length];
+        for (int i = 0, k = 0; i < intArr.length; i++, k++) {
+            if (k == index) {
+                newArray[k] = value;
+                continue;
+            } else {
+                newArray[k] = intArr[i];
+            }
+        }
+        System.out.println("Array after removing index of element: " + Arrays.toString(newArray));
+    }
+
+    //8. Write a Java program to copy an array by iterating the array. Go to the editor
+    public static void copyArray(int[] intArr, int[] newArr) {
+        System.out.println("My array: " + Arrays.toString(intArr));
+        for (int i = 0; i < intArr.length; i++) {
+            newArr[i] = intArr[i];
+        }
+        System.out.println("New Array (copy) : " + Arrays.toString(newArr));
+    }
+
+    //7. Write a Java program to remove a specific element from an array. Go to the editor
+    public static void removeElement(int[] intArr, int removeIndex) {
+        System.out.println("Original Array of int: ");
+        for (int i = 0; i < intArr.length; i++) {
+            System.out.print(intArr[i] + " ");
+        }
+        System.out.println("\nIndex of element to be removed: " + removeIndex);
+        if (intArr == null) {
+            System.out.println("The list is empty!!");
+        }
+        int[] newArray = new int[intArr.length - 1];
+        for (int i = 0, k = 0; i < intArr.length; i++) {
+            if (i == removeIndex) {
+                continue;
+            } else {
+                newArray[k++] = intArr[i];
+            }
+        }
+        System.out.println("Array after removing index of element: " + Arrays.toString(newArray));
+    }
+
+    //    6. Write a Java program to find the index of an array element
+    public static int findIndex(int[] intArr, int t) {
+        if (intArr == null) {
+            return -1;
+        }
+        int len = intArr.length;
+        int i = 0;
+        while (i < len) {
+            if (intArr[i] == t) {
+                return i;
+            } else i++;
+        }
+        return -1;
+    }
+
+    //  5. Write a Java program to test if an array contains a specific value.
+    public static boolean contains(int[] intArr, int item) {
+        for (int n : intArr) {
+            if (item == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+//        //4. Write a Java program to calculate the average value of array elements.
+//        int[] intArr = {5,4,3,5};
+//        int sum = 0;
+//        int elements = 0;
+//        double average;
+//
+//        for (int i =0 ; i < intArr.length ; i++) {
+//            int temp = intArr[i];
+//            sum+=temp;
+//            elements++;
+//        }
+//        average = sum / elements;
+//        System.out.println(average);
+//
+//
+////Write a Java program to sum values of an array
+//        int[] intArr = {5,2,3,4,6};
+//        int sum =0 ;
+//
+//        for (int i =0 ; i < intArr.length; i++) {
+//            int temp = intArr[i];
+//            sum +=temp;
+//        }
+//        System.out.println(sum);
+//
+//
+//        //Sort String Array
+//        String[] myArray = {"JavaFX", "HBase", "OpenCV", "Java", "Hadoop", "Neo4j"};
+//        int size = myArray.length;
+//
+//        for(int i = 0; i<size-1; i++) {
+//            for (int j = i+1; j<myArray.length; j++) {
+//                if(myArray[i].compareTo(myArray[j])>0) {
+//                    String temp = myArray[i];
+//                    myArray[i] = myArray[j];
+//                    myArray[j] = temp;
+//                }
+//            }
+//        }
+//        System.out.println(Arrays.toString(myArray));
+//        //using Sort method of the Arrays class
+//        Arrays.sort(myArray);
+//        System.out.println(Arrays.toString(myArray));
+//
+//        //Sort Numberic Array (Bubble sort)
+//        int[] myInt = {6,5,3,2,5,7,1,2,4,53,2};
+//
+//        for (int i =0 ; i < myInt.length-1 ; i++) {
+//            for (int j=0; j< myInt.length-i-1; j++) {
+//                if(myInt[j] > myInt[j+1]) {
+//                    int temp = myInt[j];
+//                    myInt[j] = myInt[j+1];
+//                    myInt[j+1] = temp;
+//                }
+//            }
+//        }
+//        for(int i =0 ; i < myInt.length ; i++) {
+//            System.out.print(" " + myInt[i]);
+//        }
+
+}
